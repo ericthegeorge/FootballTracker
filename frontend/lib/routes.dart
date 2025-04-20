a716d51c848bbd9fda511a37cbc43a4a866ea226
Eric George <erickurian@gmail.com>
2025-04-20 15:32:52 -0600

    made base for main selection screen

--- frontend/lib/routes.dart
+++ frontend/lib/routes.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
+// import 'package:frontend/screens/main_selection_screen.dart';
 import 'screens/home_screen.dart';
 import 'screens/login_screen.dart';
 import 'screens/register_screen.dart';
+import 'screens/main_selection_screen.dart';
 
 class Routes {
   static const String home = '/home';
   static const String login = '/login';
   static const String register = '/register';
+  static const String main_selection = '/main_selection';
+  static const String leagues = '/leagues';
+  static const String teams = '/teams';
+  static const String players = '/players';
+  static const String matches = '/matches';
+  static const String account_settings = '/account_settings';
 
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
@@ -16,6 +24,8 @@ class Routes {
         return MaterialPageRoute(builder: (_) => LoginScreen());
       case register:
         return MaterialPageRoute(builder: (_) => RegisterScreen());
+      case main_selection:
+        return MaterialPageRoute(builder: (_) => MainSelectionScreen());
       default:
         return MaterialPageRoute(
           builder: (_) => HomeScreen(),
